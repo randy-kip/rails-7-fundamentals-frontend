6da0cf0eaa45f58148f2c845a841edc66458eaf8
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-14 16:11:59 +0300

    1. refactor: Edit wiki_posts in the show action to singular form because it returns one record. 2. feat: Add content for the index and show pages in the View.

--- app/controllers/wiki_posts_controller.rb
+++ app/controllers/wiki_posts_controller.rb
@@ -14,6 +14,6 @@ class WikiPostsController < ApplicationController
         uri = URI.parse("http://localhost:3000/api/v1/wiki_posts/#{id}")
         response = Net::HTTP.get_response(uri)
 
-        @wiki_posts = JSON.parse(response.body)
+        @wiki_post = JSON.parse(response.body)
     end
 end
