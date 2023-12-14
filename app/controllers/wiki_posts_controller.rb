c3d7a4a59d4eb5c301f4f33736451bd8726eeecf
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-14 15:09:55 +0300

    Set up Rails app

---
+++ app/controllers/wiki_posts_controller.rb
@@ -0,0 +1,2 @@
+class WikiPostsController < ApplicationController
+end
---
+++ test/controllers/wiki_posts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WikiPostsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
