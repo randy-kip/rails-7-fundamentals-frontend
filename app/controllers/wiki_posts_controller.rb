bc0c8399c083eac010d275a5efc37ba7342b04cd
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-14 15:39:25 +0300

    feat: Create wikiposts route and create a client request from the localhost for wikiposts data.

--- app/controllers/wiki_posts_controller.rb
+++ app/controllers/wiki_posts_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+require 'net/http'
+
 class WikiPostsController < ApplicationController
+    def index
+        uri = URI.parse('http://localhost:3000/api/v1/wiki_posts')
+        response = Net::HTTP.get_response(uri)
+
+        @wiki_posts = JSON.parse(response.body)
+    end
+
+    def show
+        id = params[:id]
+        uri = URI.parse("http://localhost:3000/api/v1/wiki_posts/#{id}")
+        response = Net::HTTP.get_response(uri)
+
+        @wiki_posts = JSON.parse(response.body)
+    end
 end
